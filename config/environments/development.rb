5e5f3322197b5539dc0246779072b17831bc1c7f
ShimazakiHiromu <142462772+ShimazakiHiromu@users.noreply.github.com>
2024-05-26 16:12:01 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,13 +45,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',  
-    user_name:            'music.diary.app@gmail.com',
-    password:             'iltt fbdl cdsn crri',
-    authentication:       'plain',
-    enable_starttls_auto: true  
+    
   }
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
