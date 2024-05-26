2dd5014312efa27a4ed36fdcc3b2e174b8a88f54
ShimazakiHiromu <shima.hiro166@gmail.com>
2024-05-26 16:32:08 +0900

    回答しました

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    
+    address:              ENV['SMTP_ADDRESS'],
+    port:                 ENV['SMTP_PORT'],
+    domain:               ENV['SMTP_DOMAIN'],
+    user_name:            ENV['SMTP_USER_NAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttlsauto:  true
   }
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
